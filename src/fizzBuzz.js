2f8bf8e347952b3dd726177541a9bbb5633bed3d
sashajustice <sabrinnasreddin@gmail.com>
2017-06-01 11:55:33 -0700

    fizzBuzz 1st draft

---
+++ src/fizzBuzz.js
@@ -0,0 +1,15 @@
+function fizzBuzz (arr) {
+  var points = new Array(100);
+        for (var i = 0; i < 100; i++) {
+            points[i] = i + 1;
+         if(arr % 3 === 0) {
+           console.log (Fizz);
+         else if( arr % 5 === 0) {
+           console.log (Buzz);
+         } else {
+           (console.log(FizzBuzz);
+         }
+         return points;
+        }
+      }
+}
