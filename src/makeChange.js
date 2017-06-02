2fefbf75f440e2ff9e6dedde93d679b5c61225cb
Aileen Santos <aileensantos@LGs-MacBook-Pro-2.local>
2017-06-02 16:50:20 -0700

    All functions are complete, except for makeChange, closesPair, and isConnectedGraph

--- src/makeChange.js
+++ src/makeChange.js
@@ -1,3 +1,24 @@
-export default function makeChange({price, amountGiven}) {
-  // your code here
-}
+function makeChange(coins, total){
+  var count = 0;
+
+  var changer = function(index, value){
+
+    var currentCoin = coins[index];
+
+    if( index === 0){
+      if( value % currentCoin === 0){
+        count++;
+      }
+      return;
+    }
+
+    while( value >= 0 ){
+      changer(index-1, value);
+      value -= currentCoin;
+    }
+  }
+  changer(coins.length-1, total);
+  return count;
+};
+
+export default makeChange
